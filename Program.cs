f0da0dedf869a137a023c3639b23d247791a7979
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-01-21 08:46:23 -0500

    Prevent the Console from opening by waiting for the user to press a key.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, C# World!");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
 
