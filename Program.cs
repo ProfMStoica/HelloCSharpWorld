111f86bfe2ff730592b430ba32f2cd5c35bb7db9
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-01-24 12:20:48 -0500

    Add more comments about closing of the program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
     {
         //Print a hello world message to the user
         Console.WriteLine("Hello, C# World!");
+        
+        //Wait for the user to press a key before closing the program
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
     }
