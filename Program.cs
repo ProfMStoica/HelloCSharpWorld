aa03ea3985d27c38b0f2898d0fe06d6a4e686e0e
Magdin (Personal) <magdin.stoica@icloud.com>
2025-01-24 12:21:22 -0500

    Remove all comments.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,7 @@
 {
     public static void Main(string[] args)
     {
-        //Print a hello world message to the user
         Console.WriteLine("Hello, C# World!");
-        
-        //Wait for the user to press a key before closing the program
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
     }
